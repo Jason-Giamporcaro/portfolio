58fbd99dff346724269d51fd16e6f02a566812d0
Giamporcaro Jonathan <jonathan.giamporcaro@gmail.com>
2017-04-05 18:45:15 +0200

    add youtube embed

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@
    function resizeVideo(width) {
      var diff = width * 0.032;
      var widthAttribute = width - diff;
-     var heightAttribute = width *9 / 16 - diff * 9 / 16;
+     var heightAttribute = width * 9 / 16 - diff * 9 / 16;
      var windowHeight = window.innerHeight
       || document.documentElement.clientHeight
       || document.body.clientHeight;
@@ -29,13 +29,14 @@
    function loadVideo(video) {
      modal.style.display = "block";
      resizeVideo(playerContainer.offsetWidth)
-     player.src = "http://jonathan-giamporcaro.com/videos/"+video+".mp4";
+     player.src = "https://www.youtube.com/embed/"+video;
    }
 
    window.onclick = function(event) {
     if(event.target !== modal)
       return;
-    player.pause();
+    player.setAttribute('src', '');
+    // player.pause();
     modal.style.display = "none";
    }
 })();
